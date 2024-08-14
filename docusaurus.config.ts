815d64d5806694bf363745042d797a2a95064050
Boris Heithecker <boris.heithecker@gmx.net>
2024-08-14 10:59:51 +0200

    Nicht benötigte 'Docusaurus social card' entfernt.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,8 +58,6 @@ const config: Config = {
 
   themeConfig:
     {
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Schulconnex',
         logo: {
