020a7b540126db87235965572cfb727daa8d5c23
Alexander Futász <alexander.futasz@fokus.fraunhofer.de>
2024-09-05 11:08:36 +0200

    Throw bei broken markdown-Links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,8 +20,8 @@ const config: Config = {
   organizationName: 'schulconnex.github.io', // Usually your GitHub org/user name.
   projectName: 'schulconnex', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
