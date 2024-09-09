d0be66e34a150d088eb89e0f32446593b5d2c85e
Boris Heithecker <boris.heithecker@gmx.net>
2024-09-09 10:47:48 +0200

    194: Testimonials initialer Commit.

---
+++ src/data/testimonials.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable global-require */
+
+import React from 'react';
+import Translate from '@docusaurus/Translate';
+
+export type TestimonialItem = {
+  image: {
+    src: string;
+    width?: number;
+    height?: number;
+  };
+  text: JSX.Element;
+  quote: {
+    name: string;
+    info: string;
+  };
+  showOnHomepage: boolean;
+  thumbnail?;
+  title?: string;
+};
+
+const TESTIMONIALS: TestimonialItem[] = [
+  {
+    image: {
+      src: '/img/lotus-flower-1805784_640.png',
+      width: 200,
+    },
+    text: (
+        <Translate id="homepage.testimonials.name1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Translate>
+    ),
+    quote: {
+        name: 'Erika Musterfrau',
+        info: 'Product Ownerin X',
+    },
+    showOnHomepage: true,
+  },
+  {
+    image: {
+      src: '/img/tick-763800_640.png',
+      width: 200,
+    },
+    text: (
+        <Translate id="homepage.testimonials.name2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Translate>
+    ),
+    quote: {
+      name: 'Hans Meyer',
+      info: 'Firmenname AG',
+    },
+    showOnHomepage: true,
+  },
+  {
+    image: {
+      src: '/img/lotus-flower-1805784_640.png',
+      width: 200,
+    },
+    text: (
+        <Translate id="homepage.testimonials.name3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </Translate>
+    ),
+    quote: {
+      name: 'Max Mustermann jr.',
+      info: 'Entwickler bei Firma X',
+    },
+    showOnHomepage: true,
+  },
+  {
+    image: {
+      src: '/img/company-7759278_640.png',
+      width: 200,
+    },
+    text: (
+        <Translate id="homepage.testimonials.name4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Translate>
+    ),
+    quote: {
+      name: 'Max Mustermann',
+      info: 'Entwickler bei Unternehmen GmbH',
+    },
+    showOnHomepage: true,
+  },
+  {
+    image: {
+      src: '/img/star-2822548_640.png',
+      width: 200,
+    },
+    text: (
+        <Translate id="homepage.testimonials.name5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Translate>
+    ),
+    quote: {
+      name: 'Maxine Musterfrau',
+      info: 'Product Ownerin B',
+    },
+    showOnHomepage: true,
+  },
+];
+
+export default TESTIMONIALS;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,15 @@ import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+import { useBaseUrlUtils } from '@docusaurus/useBaseUrl';
+
 import HomepageText from './_homepage-text.md';
 
+import Testimonials from // type TestimonialItem,
+'@site/src/data/testimonials';
+
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -22,17 +27,72 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+function Testimonial(
+  { feature: testimonial, className },
+  // : {
+  //   feature: TestimonialItem;
+  //   className?: string;
+  // }
+) {
+  const { withBaseUrl } = useBaseUrlUtils();
+
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="">
+    <div className={clsx('col', className)}>
+      <img
+        className={styles.featureImage}
+        alt={testimonial.title}
+        width={Math.floor(testimonial.image.width)}
+        height={Math.floor(testimonial.image.height)}
+        src={withBaseUrl(testimonial.image.src)}
+        loading="lazy"
+      />
+      {/* <Heading as="h3" className={clsx(styles.testimonialsHeading)}>
+        {testimonial.title}
+      </Heading> */}
+      <p className="padding-horiz--md">{testimonial.text}</p>
+      <p className="padding-horiz--md text--right text--italic margin-bottom--none">
+        {testimonial.quote.name}
+      </p>
+      <p className="padding-horiz--md text--right text--italic">
+        {testimonial.quote.info}
+      </p>
+    </div>
+  );
+}
+
+function TestimonialsContainer() {
+  const firstRow = Testimonials.slice(0, 3);
+  const secondRow = Testimonials.slice(3);
+
+  return (
+    <div className="container text--center">
+      <div className="row margin-top--lg margin-bottom--lg">
+        {firstRow.map((feature, idx) => (
+          <Testimonial feature={feature} key={idx} />
+        ))}
+      </div>
+      <div className="row">
+        {secondRow.map((feature, idx) => (
+          <Testimonial
+            feature={feature}
+            key={idx}
+            className={clsx('col--4', idx === 0 && 'col--offset-2')}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`} description="">
       <HomepageHeader />
       <main>
-        <div class="container">
-          <div className='row padding-top--lg padding-top--lg padding-bottom--md'>
-            <div className='col'>
+        <div className="container">
+          <div className="row padding-top--lg padding-top--lg padding-bottom--md">
+            <div className="col">
               <article>
                 <div className="theme-doc-markdown markdown">
                   <HomepageText />
@@ -40,17 +100,21 @@ export default function Home() {
               </article>
             </div>
           </div>
-          <div className='row padding-top--lg padding-bottom--xl'>
-            <div className='col'>
+          <div className="row padding-top--lg padding-bottom--xl">
+            <div className="col">
               <div className={styles.buttons}>
                 <Link
                   className="button button--secondary button--lg"
-                  to={`/docs/${siteConfig.presets[0][1].docs.versions.current.path}/stand-der-bearbeitung`}>
+                  to={`/docs/${siteConfig.presets[0][1].docs.versions.current.path}/stand-der-bearbeitung`}
+                >
                   zur Spezifikation
                 </Link>
               </div>
             </div>
           </div>
+        </div>
+        <div className={styles.section}>
+          <TestimonialsContainer />
         </div>
       </main>
     </Layout>
