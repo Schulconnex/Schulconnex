11784a228d66f3a187b46ba92731931eb4daf1eb
Boris Heithecker <boris.heithecker@gmx.net>
2024-09-23 18:19:29 +0200

    194: Testimonials verkürzt.

--- src/data/testimonials.tsx
+++ src/data/testimonials.tsx
@@ -30,10 +30,7 @@ const TESTIMONIALS: TestimonialItem[] = [
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        commodo consequat.
       </Translate>
     ),
     quote: {
@@ -87,12 +84,7 @@ const TESTIMONIALS: TestimonialItem[] = [
     text: (
         <Translate id="homepage.testimonials.name4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        tempor incididunt ut labore et dolore magna aliqua.
       </Translate>
     ),
     quote: {
