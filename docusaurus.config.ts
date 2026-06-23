569fc7f23e64253238060e69bc8d60fcf4776308
bwhdien <114562726+bwhdien@users.noreply.github.com>
2026-06-23 08:34:54 +0200

    Added Version switch for 1.8-rc1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,10 @@ const config: Config = {
               label: 'Version 1.7',
               // path: '1.5',
             },
+              '1.8-rc1': { 
+              label: 'Version 1.8 Veröffentlichungskandidat 1', 
+              banner: 'unreleased' 
+            },
           },
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
